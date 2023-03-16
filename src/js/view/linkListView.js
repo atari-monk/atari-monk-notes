02bdfece0944ebe242505b487a599999f7ad90cf
atari-monk <atari.monk1@gmail.com>
2023-03-16 13:04:57 +0100

    index nav links fix

--- src/js/view/linkListView.js
+++ src/js/view/linkListView.js
@@ -23,6 +23,7 @@ export class LinkListView extends View {
   #buildSection(data, sectionEl) {
     const newSection = this._getNewParent(sectionEl);
     this._templateText(newSection, 'h3', 'title', data.title);
+    this._setAttribute(data, 'navId', newSection, 'id');
     return newSection;
   }
 
