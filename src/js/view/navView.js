8aefa56d6c09432808325fa8499a59ee11fc8cc5
atari-monk <atari.monk1@gmail.com>
2023-03-17 17:25:22 +0100

    refactor navView

--- src/js/view/navView.js
+++ src/js/view/navView.js
@@ -4,7 +4,7 @@ export class NavView extends View {
   #navList;
 
   createContent(data, detectOSCard, styleBtn) {
-    const nav = this.#createNav(data);
+    const nav = this.#createNav();
     this.#navList = nav.querySelector('.nav-ul');
     this.#createNavLinks(data);
     this.#createDetectOsCard(nav, detectOSCard);
@@ -14,24 +14,22 @@ export class NavView extends View {
   }
 
   #createTitle(data) {
-    const title = this._getParentElement('template-title', '.title');
-    const newtitle = this._getNewParent(title);
+    const newtitle = this._getNewParent(
+      this._getParentElement('template-title', '.title')
+    );
     this._templateText(newtitle, '.title-title', 'title', data.title);
     return newtitle;
   }
 
-  #createNav(data) {
-    const nav = this._getParentElement('template-nav', '.nav');
-    const newNav = this._getNewParent(nav);
-    // this._templateText(newNav, '.nav-title', 'title', data.title);
-    return newNav;
+  #createNav() {
+    return this._getNewParent(this._getParentElement('template-nav', '.nav'));
   }
 
   #createNavLinks(data) {
-    const navLinks = this._filterOne(data.notes, 'nav');
-    const navItem = this._getParentElement('template-nav-item', '.nav-item');
-    navLinks.nav.forEach((navData) => {
-      const newNavItem = this._getNewParent(navItem);
+    data.nav.forEach((navData) => {
+      const newNavItem = this._getNewParent(
+        this._getParentElement('template-nav-item', '.nav-item')
+      );
       this._templateLink(
         newNavItem,
         'nav_link',
