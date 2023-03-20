b882538ab5b880549a251588c3cb0ed500912a51
atari-monk <atari.monk1@gmail.com>
2023-03-20 15:59:23 +0100

    turn off debug

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const CONNECTION =
   'https://atari-monk.github.io/js-notes-json/src/json-v2/';
-export const DEBUG = true;
+export const DEBUG = false;
 export const SYSTEM = {
   Unknown: 'Unknown',
   Android: 'Android',
