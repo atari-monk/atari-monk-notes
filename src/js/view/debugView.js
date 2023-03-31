e550e5f1b8a02ca597d5fb29e12822d0b0a4fab4
atari-monk <atari.monk1@gmail.com>
2023-03-31 20:49:19 +0200

    responsive tag

--- src/js/view/debugView.js
+++ src/js/view/debugView.js
@@ -8,8 +8,13 @@ export class DebugView extends View {
   #createView() {
     const viewEl = this._getParentElement('template-debug', '.debug');
     const newView = this._getNewParent(viewEl);
-    this._templateChildHtml2(newView, '.debug-log', 'log', 'Debug Info');
+    this._templateChildHtml2(newView, '.debug-log', 'log', this.#getInfo());
     document.body.appendChild(newView);
+  }
+
+  #getInfo() {
+    return `orientation: ${screen.orientation.type}, angle: ${screen.orientation.angle}, width: ${screen.width}, height: ${screen.height}
+    `;
   }
 }
 
