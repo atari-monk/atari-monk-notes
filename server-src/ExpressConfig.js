5a2c0ad2ca86acd985d4f252b124f62feeefa085
atari-monk <atari.monk1@gmail.com>
2023-04-23 01:49:37 +0200

    micro refactor

--- server-src/ExpressConfig.js
+++ server-src/ExpressConfig.js
@@ -3,27 +3,25 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 class ExpressConfig {
+  #app;
+
   constructor() {
-    this.app = express();
-    this.app.use(express.static('public'));
-    //this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.set('views', path.join(__dirname, 'views'));
-    this.app.set('view engine', 'ejs');
+    this.#app = express();
+    this.#app.use(express.static('public'));
+    this.#app.use(bodyParser.urlencoded({ extended: true }));
+    this.#app.set('views', path.join(__dirname, 'views'));
+    this.#app.set('view engine', 'ejs');
   }
 
   get app() {
-    return this._app;
-  }
-
-  set app(value) {
-    this._app = value;
+    return this.#app;
   }
 
   start(port) {
-    this.app.listen(port, () => {
+    this.#app.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
   }
 }
+
 exports.ExpressConfig = ExpressConfig;
