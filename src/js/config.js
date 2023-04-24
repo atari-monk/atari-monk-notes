c7a2528d67dd267d0c73b6efc0c0690a8139f445
atari-monk <atari.monk1@gmail.com>
2023-04-24 17:15:44 +0200

    remove prototypes

--- src/js/config.js
+++ src/js/config.js
@@ -1,15 +1,15 @@
-/*
+///*
 export const CONNECTION =
   'http://127.0.0.1:5501/src/json/';
 export const DEBUG = true;
 export const HOME = '/';
-*/
-///*
+//*/
+/*
 export const CONNECTION =
   'https://atari-monk.github.io/js-notes-json/src/json-v2/';
 export const DEBUG = false;
 export const HOME = 'https://atari-monk.github.io/js-notes-templated/';
-//*/
+*/
 export const SYSTEM = {
   Unknown: 'Unknown',
   Android: 'Android',
