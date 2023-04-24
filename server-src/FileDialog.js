eaf7d0c5b4a8a2449ed1ce9d5eaf37903d2699f2
atari-monk <atari.monk1@gmail.com>
2023-04-24 17:24:56 +0200

    refactor to use async await

--- server-src/FileDialog.js
+++ server-src/FileDialog.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 class FileDialog {
@@ -8,40 +8,43 @@ class FileDialog {
     this.#dirs = [];
   }
 
-  //this handles directories
-  handleRequest(req, res) {
+  // this handles directories
+  async handleRequest(req, res) {
     let fileName = req.query.directoryPath;
     if (fileName === undefined) fileName = this.root;
-    //fileName = fileName.replace(/\\/g, '/');
+    fileName = fileName.replace(/\\/g, '/');
     this.#dirs.push(fileName);
-    fs.readdir(fileName, (err, files) => {
-      if (err) {
-        console.error(`Error reading directory: ${fileName}`);
-        res.status(500).send('Error reading directory');
-        return;
-      }
-
-      files = files.filter((file) => !file.startsWith('.'));
-      const filepaths = files.map((file) => path.join(fileName, file));
-      const types = filepaths.map((filepath) => {
-        return fs.statSync(filepath).isDirectory() ? 'directory' : 'file';
-      });
-      res.render('file-dialog', { filepaths, files, types });
-    });
+    try {
+      const files = await fs.readdir(fileName);
+      const filteredFiles = files.filter((file) => !file.startsWith('.'));
+      const filepaths = filteredFiles.map((file) => path.join(fileName, file));
+      const types = await Promise.all(
+        filepaths.map(async (filepath) => {
+          const stats = await fs.stat(filepath);
+          return stats.isDirectory() ? 'directory' : 'file';
+        })
+      );
+      res.render('file-dialog', { filepaths, files: filteredFiles, types });
+    } catch (err) {
+      console.error(`Error reading directory: ${fileName}`);
+      res.status(500).send('Error reading directory');
+    }
   }
 
-  //this handles files
-  handleSelected(req, res) {
+  // this handles files
+  async handleSelected(req, res) {
     let fileName = req.query.file;
     if (fileName === undefined) {
       console.log('Error! No file name!');
       res.status(400).send('Error! No file name provided.');
       return;
     }
-    const filePath = path.join(this.#dirs.pop(), fileName);
+    const dirName = this.#dirs.pop();
+    const filePath = path.join(dirName, fileName);
     console.log('FileDialog -> File name:', fileName);
     console.log('FileDialog -> File path:', filePath);
     res.render('file-menu', { fileName, filePath });
   }
 }
+
 exports.FileDialog = FileDialog;
