8932784626e84c9d3d5d848ff3fc0a2d929157e9
atari-monk <atari.monk1@gmail.com>
2023-05-01 13:18:45 +0200

    upd config

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,13 @@
 /*
 export const CONNECTION =
-  'http://127.0.0.1:5501/src/json/';
+  'http://127.0.0.1:5501/db/json-v2/';
 export const DEBUG = true;
 export const HOME = '/';
 */
 ///*
-export const CONNECTION =
-  'https://atari-monk.github.io/js-notes-json/src/json-v2/';
+export const CONNECTION = 'https://atari-monk.github.io/atari-monk-notes/db/json-v2/';
 export const DEBUG = false;
-export const HOME = 'https://atari-monk.github.io/js-notes-templated/';
+export const HOME = 'https://atari-monk.github.io/atari-monk-notes/';
 //*/
 export const SYSTEM = {
   Unknown: 'Unknown',
